a396cafb90ac8189049521d0b73610c73028cb81
Sudhir Hadge <sudhir.hadge@gmail.com>
2026-05-06 22:22:53 +0530

    (fix) :Footer not sticking to bottom & layout inconsistency

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,14 +29,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <div className="flex-1">
-          <Header />
-        </div>
-        {children}
-        <div className="flex-1">
-          <Footer />
-        </div>
+      <body className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1" >{children}</main>
+        <Footer />
       </body>
     </html >
   );
